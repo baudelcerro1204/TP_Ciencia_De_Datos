f984a236ad164d9e014ba905fa5e640e8039aacf
Ramiro <ralanci@uade.edu.ar>
2025-06-10 15:01:58 -0300

    notebook exploratorio

--- src/scripts/download_dataset.py
+++ src/scripts/download_dataset.py
@@ -2,21 +2,25 @@ import os
 import zipfile
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-# Inicialización del API
-api = KaggleApi()
-api.authenticate()
+def download_kaggle_dataset():
+    # Inicialización del API
+    api = KaggleApi()
+    api.authenticate()
 
-# Dataset que querés descargar
-dataset = "devdope/900k-spotify"
+    # Dataset a descargar
+    dataset = "devdope/900k-spotify"
 
-# Carpeta donde lo vas a guardar
-download_path = os.path.join("data", "raw")
-os.makedirs(download_path, exist_ok=True)
+    # Ruta destino
+    download_path = os.path.join("data", "raw")
+    os.makedirs(download_path, exist_ok=True)
 
-# Descargar el dataset ZIP
-print("Descargando dataset desde Kaggle...")
-api.dataset_download_files(dataset, path=download_path, unzip=True)
-print("Descarga completada.")
+    # Descargar y descomprimir
+    print("Descargando dataset desde Kaggle...")
+    api.dataset_download_files(dataset, path=download_path, unzip=True)
+    print("Descarga completada.")
 
-# Mostrar los archivos descargados
-print("Archivos en", download_path, ":", os.listdir(download_path))
+    # Mostrar archivos
+    print("Archivos en", download_path, ":", os.listdir(download_path))
+
+if __name__ == "__main__":
+    download_kaggle_dataset()
