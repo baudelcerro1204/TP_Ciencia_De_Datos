fc824c22bea26c116f588f857bc85603d9c6496f
baudelcerro1204 <bdelcerro@uade.edu.ar>
2025-06-16 11:34:33 -0300

    empezar de 0

--- src/server/main.py
+++ src/server/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from src.api.routers import predict
+from src.api.routers import predict, audio_features
 
 app = FastAPI()
 
 app.include_router(predict.router)
+app.include_router(audio_features.router)
 
 @app.get("/")
 def read_root():
